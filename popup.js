a5872833a3e621b3d48d326cc763eb772bb6d6b0
Ismail Sacic <>
2021-02-05 15:06:19 +0100

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,9 +1,17 @@
 var value = "Hello world";
 
-chrome.storage.sync.set({key: value}, function() {
-    console.log('Value is set to ' + value);
-});
+// chrome.storage.sync.set({key: value}, function() {
+//     console.log('Value is set to ' + value);
+// });
 
-chrome.storage.sync.get(['key'], function(result) {
-    console.log('Value currently is ' + result.key);
-});
+// chrome.storage.sync.get(['key'], function(result) {
+//     console.log('Value currently is ' + result.key);
+// });
+
+
+localStorage.setItem('myCat', 'Tom');
+
+var cat = localStorage.getItem('myCat');
+
+console.log(cat)
+
