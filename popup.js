1059f0f44b397f691cedb77e8d0edc0981e14640
Ismail Sacic <>
2021-02-05 13:29:54 +0100

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,1 +1,9 @@
-// Future JavaScript will go here
+var value = "Hello world";
+
+chrome.storage.sync.set({key: value}, function() {
+    console.log('Value is set to ' + value);
+});
+
+chrome.storage.sync.get(['key'], function(result) {
+    console.log('Value currently is ' + result.key);
+});
